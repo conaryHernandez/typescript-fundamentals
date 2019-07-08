2aa4770627af88e1529b9d35abfd1f863122df55
Conary <31629328+conaryHernandez@users.noreply.github.com>
2019-07-07 18:54:27 -0600

    first exercises

---
+++ types.js
@@ -0,0 +1,6 @@
+console.log('hello from ts');
+var myString;
+var myNum;
+myString = 'learning typescript';
+myNum = 23;
+console.log(myString, 23);
---
+++ types.ts
@@ -0,0 +1,9 @@
+console.log('hello from ts');
+
+let myString: string;
+let myNum: number;
+
+myString = 'learning typescript';
+myNum = 23;
+
+console.log(myString, 23);
