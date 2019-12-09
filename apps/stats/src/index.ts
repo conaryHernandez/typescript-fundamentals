60bc24779dddb65022e383c362ec8928de639ffc
Conary <31629328+conaryHernandez@users.noreply.github.com>
2019-12-08 20:55:43 -0600

    reading csv file

--- apps/stats/src/index.ts
+++ apps/stats/src/index.ts
@@ -1,3 +1,12 @@
 import fs from 'fs';
 
-console.log('helloo');
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8' //  without this, it would give us a buffer with raw data
+  })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+console.log(matches);
