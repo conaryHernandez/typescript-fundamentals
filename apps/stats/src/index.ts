60624edc61c100cbe2bec5c7e650937c17af90c8
Conary <31629328+conaryHernandez@users.noreply.github.com>
2019-12-09 21:19:43 -0600

    added MatchData enum, utils file and CsvFielReador .bak file

--- apps/stats/src/index.ts
+++ apps/stats/src/index.ts
@@ -1,12 +1,20 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResult';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8' //  without this, it would give us a buffer with raw data
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+const reader = new CsvFileReader('football.csv');
 
-console.log(matches);
+reader.read();
+
+const firstMatch = reader.data[0][0];
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
