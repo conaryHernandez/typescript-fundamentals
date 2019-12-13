0ea2db00d17887a954f060df34425ef63e7e1138
conaryh <conaryh@gmail.com>
2019-12-13 16:26:59 -0600

    added event handling and json server

--- apps/web/src/index.ts
+++ apps/web/src/index.ts
@@ -1,5 +1,10 @@
 import { User } from './models/User';
+import axios from 'axios';
 
 const user = new User({ name: 'Conary', age: 24 });
 
-console.log(user.get('name'));
+user.on('change', () => {
+  console.log('Change #1');
+});
+
+axios.get('http://localhost:3000/users/1');
