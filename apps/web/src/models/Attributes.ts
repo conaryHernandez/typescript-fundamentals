c79906246b6ce3c22bc2810794be3a680be4e388
conaryh <conaryh@gmail.com>
2019-12-18 15:06:22 -0600

    Added attributes model

---
+++ apps/web/src/models/Attributes.ts
@@ -0,0 +1,21 @@
+import { UserProps } from './User';
+
+export class Attributes<T> {
+  constructor(private data: T) {}
+
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
+  }
+
+  set(update: T): void {
+    Object.assign(this.data, update);
+  }
+}
+
+const attrs = new Attributes<UserProps>({
+  id: 5,
+  age: 20,
+  name: 'asf'
+});
+
+const name = attrs.get('name');
--- apps/web/src/models/User.ts
+++ apps/web/src/models/User.ts
@@ -1,4 +1,5 @@
 import { Eventing } from './Eventing';
+import { Sync } from './Sync';
 
 export interface UserProps {
   id?: number;
@@ -6,16 +7,9 @@ export interface UserProps {
   age?: number;
 }
 
+const rootUrl = 'http://localhost:3000/users';
+
 export class User {
   public events: Eventing = new Eventing();
-
-  constructor(private data: UserProps) {}
-
-  get(propName: string): string | number {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 }
