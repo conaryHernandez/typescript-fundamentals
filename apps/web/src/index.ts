55cdd3d149048c4f7c6d9038656bcc225794bcba
conaryh <conaryh@gmail.com>
2020-01-02 10:33:01 -0600

    Fetching user data, saving data, extracting model class

--- apps/web/src/index.ts
+++ apps/web/src/index.ts
@@ -1,10 +1,10 @@
 import { User } from './models/User';
 import axios from 'axios';
 
-const user = new User({ name: 'new record', age: 0 });
+const user = new User({ id: 1, name: 'newer name', age: 0 });
 
-user.on('change', () => {
-  console.log('change!!!');
+user.on('save', () => {
+  console.log(user);
 });
 
-console.log(user.get('name'));
+user.save();
--- apps/web/src/models/Attributes.ts
+++ apps/web/src/models/Attributes.ts
@@ -1,11 +1,16 @@
 export class Attributes<T> {
   constructor(private data: T) {}
 
+  //k extends keyof Generic means will be one posible key of this type
   get = <K extends keyof T>(key: K): T[K] => {
     return this.data[key];
   };
 
   set(update: T): void {
     Object.assign(this.data, update);
+  }
+
+  getAll(): T {
+    return this.data;
   }
 }
