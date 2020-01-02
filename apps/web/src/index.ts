998e04987848cbc4141cc294aac1d61a2d8ff33f
conaryh <conaryh@gmail.com>
2020-01-02 11:25:01 -0600

    added collection model

--- apps/web/src/index.ts
+++ apps/web/src/index.ts
@@ -1,9 +1,13 @@
-import { User } from './models/User';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
 
-const user = User.buildUser({ id: 3 });
+const collection = new Collection<
+  User,
+  UserProps
+>('http://localhost:3000/users', (json: UserProps) => User.buildUser(json));
 
-user.on('change', () => {
-  console.log(user);
+collection.on('change', () => {
+  console.log(collection);
 });
 
-user.fetch();
+collection.fetch();
