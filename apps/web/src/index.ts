dd99ea3934dbc9ecc765dd430c52c49ffc21b995
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-01-08 19:21:35 -0600

    binding event handlers

--- apps/web/src/index.ts
+++ apps/web/src/index.ts
@@ -1,6 +1,9 @@
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
-const userForm = new UserForm(document.getElementById('root'));
+const user = User.buildUser({ name: 'test', age: 20 });
+
+const userForm = new UserForm(document.getElementById('root'), user);
 
 userForm.render();
 
--- apps/web/src/views/UserForm.ts
+++ apps/web/src/views/UserForm.ts
@@ -1,5 +1,7 @@
+import { User } from '../models/User';
+
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {}
 
   eventsMap(): { [key: string]: () => void } {
     return {
@@ -15,15 +17,31 @@ export class UserForm {
     return `
       <div>
         <h1>User From</h1>
+        <div>User name: ${this.model.get('name')}</div>
+        <div>User age: ${this.model.get('age')}</div>
         <input />
         <button>Click me</button>
       </div>
     `;
   }
 
+  bindEvents(fragment: DocumentFragment) {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach(element => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
   render(): void {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
 
     this.parent.append(templateElement.content);
   }
