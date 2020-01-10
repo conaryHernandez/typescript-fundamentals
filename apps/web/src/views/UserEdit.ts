9b2b25b90c958ee7399025701857f1287900f1c3
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-01-09 19:00:08 -0600

    savind data from a view, created userEdit and UserShow

---
+++ apps/web/src/views/UserEdit.ts
@@ -0,0 +1,13 @@
+import { User, UserProps } from '../models/User';
+import { View } from '../views/View';
+
+export class UserEdit extends View<User, UserProps> {
+  template(): string {
+    return `
+      <div> 
+        <div class=""user-show"></div>
+        <div class=""user-form"></div>
+      </div>
+    `;
+  }
+}
