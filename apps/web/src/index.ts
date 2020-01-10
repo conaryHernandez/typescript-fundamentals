b1527c6663d924ccb5a5206750eb3f8835432e9e
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-01-09 19:52:42 -0600

    collectionView Implementation

--- apps/web/src/index.ts
+++ apps/web/src/index.ts
@@ -1,31 +1,20 @@
-import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { UserProps, User } from './models/User';
 
-const user = User.buildUser({ name: 'test', age: 20 });
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
-const root = document.getElementById('root');
+users.on('change', () => {
+  const root = document.getElementById('root');
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
-
-  userEdit.render();
-} else {
-  throw new Error('Root element nor found');
-}
-
-/* import { User } from './models/User';
-
-const collection = new Collection<
-  User,
-  UserProps
->('http://localhost:3000/users', (json: UserProps) => User.buildUser(json));
-
-
-const collection = User.buildUserCollection();
-
-collection.on('change', () => {
-  console.log(collection);
+  if (root) {
+    new UserList(root, users).render();
+  }
 });
 
-collection.fetch();
-*/
+users.fetch();
